1f254fa1abd59ad00cfdc44d969a12f6e053cc05
Juan Manuel Gimenez <juanmauel20@gmail.com>
2023-03-20 16:23:39 -0300

    dev + ejercicios faltantes se subo correctamente

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,1 +1,41 @@
 //3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+
+
+let bandera = 0;
+let resultado = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+do {
+  let dado1 = parseInt(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+  let dado2 = parseInt(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+  let sumaDados = dado1 + dado2;
+  for (let i = 0; i < 12; i++) {
+    if (sumaDados === i + 2) {
+      resultado[i]++;
+      i = 12;
+    }
+  }
+  bandera++;
+} while (bandera < 50);
+document.write(`<table>`);
+document.write(`<thead>`);
+document.write(`<tr>`);
+document.write(`<th>Suma</th>`);
+document.write(`<th>Apariciones</th>`);
+document.write(`</tr>`);
+document.write(`</thead>`);
+document.write(`<tbody>`);
+for (let numerosFilas = 0; numerosFilas < resultado.length; numerosFilas++) {
+  document.write(`<tr>`);
+  for (let numerosColumnas = 0; numerosColumnas < 2; numerosColumnas++) {
+    document.write(`<th>`);
+    if (numerosColumnas === 0) {
+      document.write(`${numerosFilas + 2}`);
+    } else {
+      let cuenta = resultado[numerosFilas];
+      document.write(`${cuenta}`);
+    }
+    document.write(`</th>`);
+  }
+  document.write(`</tr>`);
+}
+document.write(`</tbody>`);
+document.write(`</table>`);
--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -5,3 +5,9 @@
 //lado A = 24
 //lado B = 5
 //Output: 58 
+
+
+const perimetro = (altura,base) => 2*(altura+base);
+let ladoA = parseInt(prompt('Ingrese el valor de un lado del triangulo'));
+let ladoB = parseInt(prompt('Ingrese el otro valor de un lado del triangulo'));
+document.write(`El perimetro de su triangulo es de ${perimetro(ladoA,ladoB)}`)
